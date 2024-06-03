0a156a23106e2e0f49bbe9f233bcf3c061f9d537
Ishan Gurjar <ishangurjar144@gmail.com>
2024-06-03 13:21:57 +0530

    config / typo modified

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // config.js
 // const baseURL = "http://localhost:4040/api/v1/";
-const baseURL = "http://34.228.155.207:4040/api/v1";
+const baseURL = "http://34.228.155.207:4040/api/v1/";
 export default baseURL;
