a77f34de2f72fc372289d93dcd9ff89b074292ef
Ishan Gurjar <ishangurjar144@gmail.com>
2024-06-03 14:04:22 +0530

    https backend added

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // config.js
 // const baseURL = "http://localhost:4040/api/v1/";
-const baseURL = "http://34.228.155.207:4040/api/v1/";
+const baseURL = "https://home-rent-4da4.onrender.com/";
 export default baseURL;
